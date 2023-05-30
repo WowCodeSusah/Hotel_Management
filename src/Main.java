5c9c2b8600bf4ad3fe581efd38dfd879a1ca91b7
WowCodeSusah <114371708+WowCodeSusah@users.noreply.github.com>
2023-05-30 16:06:24 +0700

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@ import java.util.HashMap;
 import java.util.Random;
 
 public class Main {
+    public static ArrayList<String> keys = new ArrayList<>();
     public static void main(String[] args) {
         HashMap<String, Room> Hash_hotel = new HashMap<>();
         ArrayList<Room> Array_hotel = new ArrayList<>();
         createRooms_array(Array_hotel);
         createRooms_hashmap(Hash_hotel);
-        System.out.println(Hash_hotel);
+        searchRoom(Hash_hotel, 5, "Low");
         GUI gui = new GUI();
     }
     public static void createRooms_array(ArrayList<Room> yes) {
@@ -44,8 +45,8 @@ public class Main {
             } else {
                 condition = "Usable";
             }
-            int random_people_upperbound = 11;
-            int random_people = random.nextInt(5, random_people_upperbound);
+            int random_people_upperbound = 8;
+            int random_people = random.nextInt(2, random_people_upperbound);
             Room room = new Room(i + 1, status, price, random_people, condition);
             yes.add(room);
         }
@@ -84,14 +85,62 @@ public class Main {
             } else {
                 condition = "Usable";
             }
-            int random_people_upperbound = 11;
-            int random_people = random.nextInt(5, random_people_upperbound);
+            int random_people_upperbound = 8;
+            int random_people = random.nextInt(2, random_people_upperbound);
             Room room = new Room(i + 1, status, price, random_people, condition);
 
             // Hash Function
             String key = "";
-            key = "H" + (i + 1) + "P" + (random_price + 1) + "MP" + random_people;
+            String roomNumber = "";
+            if ((i + 1) <= 9) {
+                roomNumber = "000" + (i + 1);
+            } else if ((i + 1) <= 99 ) {
+                roomNumber = "00" + (i + 1);
+            } else if ((i + 1) <= 999) {
+                roomNumber = "0" + (i + 1);
+            } else {
+                roomNumber = "" + (i + 1);
+            }
+            key = "H" + roomNumber + "P" + (random_price + 1) + "MP" + random_people;
+            keys.add(key);
             yes.put(key, room);
         }
+    }
+    public static void searchRoom(HashMap<String, Room> hotel, int maxPeople, String price) {
+        //Search the keys
+        ArrayList<String> acceptedKeys = new ArrayList<>();
+        for (String key : keys) {
+            String currentKeys = "";
+            int currentMaxPeople = 0;
+            currentKeys = key;
+            currentMaxPeople = currentKeys.charAt(9) - 48;
+            int currentPrice = 0;
+            String actualPrice = "";
+            currentPrice = currentKeys.charAt(6) - 48;
+            if (currentPrice == 1) {
+                actualPrice = "High";
+            } else if (currentPrice == 2) {
+                actualPrice = "Medium";
+            } else {
+                actualPrice = "Low";
+            }
+            if (currentMaxPeople >= maxPeople && price.equals(actualPrice)) {
+                acceptedKeys.add(key);
+            }
+        }
+        //Search based on the status of the room
+        ArrayList<Room> acceptedRooms = new ArrayList<>();
+        for (String acceptedKey : acceptedKeys) {
+            String currentStatus = "";
+            currentStatus = hotel.get(acceptedKey).status;
+
+            String currentCondition = "";
+            currentCondition = hotel.get(acceptedKey).condition;
+
+            if ((currentCondition.equals("New") || currentCondition.equals("Usable")) && currentStatus.equals("Free")) {
+                acceptedRooms.add(hotel.get(acceptedKey));
+            }
+        }
+        System.out.println(acceptedRooms);
     }
 }
